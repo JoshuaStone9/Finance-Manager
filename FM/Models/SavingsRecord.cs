55df6f51771a62cb15c765eb002f3dac352ebc5f
Joshua Stone <joshstone@btinternet.com>
2025-09-03 22:45:49 +0100

    Making Changes

---
+++ FM/Models/SavingsRecord.cs
@@ -0,0 +1,11 @@
+
+public class SavingsRecord
+{
+    public string Savings_ID { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Amount { get; set; }
+    public string Length { get; set; } = "One-time";
+    public DateTime Date { get; set; }           
+    public string Notes { get; set; } = "";
+}
+
